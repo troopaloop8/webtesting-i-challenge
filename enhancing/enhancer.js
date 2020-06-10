17e734f082a726ba38526b005e8107cf24aefed1
troopaloop8 <jontroop@gmail.com>
2020-06-10 12:05:43 +0900

    made functions -- need to make tests now

--- enhancing/enhancer.js
+++ enhancing/enhancer.js
@@ -6,15 +6,51 @@ module.exports = {
 };
 
 function succeed(item) {
-  return { ...item };
+  if (item.enhancement < 20) {
+    const newItem = {
+      name: item.name,
+      durability: 100,
+      enhancement: item.enhancement + 1,
+    };
+    return newItem;
+  } else {
+    return item;
+  }
 }
 
 function fail(item) {
-  return { ...item };
+  if (item.enhancement < 15) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 5,
+      enhancement: item.enhancement,
+    };
+    return newItem;
+  } else if (item.enhancement > 16) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement - 1,
+    };
+    return newItem;
+  } else if (item.enhancement > 14) {
+    const newItem = {
+      name: item.name,
+      durability: item.durability - 10,
+      enhancement: item.enhancement,
+    };
+    return newItem;
+  }
+  return item;
 }
 
 function repair(item) {
-  return { ...item };
+  const newItem = {
+    name: item.name,
+    durability: 100,
+    enhancement: item.enhancement,
+  };
+  return newItem;
 }
 
 function get(item) {
